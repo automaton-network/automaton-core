b085b6cb6312485a79b3f9040a91c5fca2127348
Asen Kovachev <akovachev@users.noreply.github.com>
2018-12-19 22:43:21 +0200

    Add AppVeyor config as .yml (#4)
    Getting both Linux & Windows versions to run on AppVeyor turned out to be a challenge.
    
    * Disabled unused libraries
    * Added appveyor.yml configuration covering both Ubuntu & VS2017
    * Marked problematic tests as flaky
    * Disabled flaky tests on Windows build

--- download_and_build_third_party_libs_windows.ps1
+++ download_and_build_third_party_libs_windows.ps1
@@ -9,6 +9,23 @@ New-Item -ItemType Directory -Path .\$LOCAL_3P
 cd .\$LOCAL_3P
 [Net.ServicePointManager]::SecurityProtocol = "tls12, tls11"
 
+#  ====== Building boost ======
+echo ("="*80)
+echo "  BUILDING boost"
+echo ("="*80)
+
+echo "Moving pre-installed boost into our tree"
+Move-Item -Path C:\Libraries\boost_1_67_0 -Destination .\boost_1_68_0
+cd boost_1_68_0
+mkdir stage
+cd stage
+Move-Item -Path ..\lib64-msvc-14.1 -Destination lib
+cd ..
+# bootstrap.bat
+# b2 --with-filesystem --with-system --with-iostreams cxxstd=14 link=static stage
+cd ..
+echo "Done."
+
 function Get-GitRepo($repo, $dir, $commit) {
   echo ("="*80)
   echo " Updating $dir from repo $repo "
@@ -49,9 +66,9 @@ function Get-Archive($url, $filename, $sha) {
 
 #  ========  Download all libraries ========
 Get-GitRepo "https://github.com/LuaJIT/LuaJIT.git" "LuaJIT" "0bf80b07b0672ce874feedcc777afe1b791ccb5a"
-Get-GitRepo "https://github.com/zeromq/libzmq.git" "libzmq" "d062edd8c142384792955796329baf1e5a3377cd"
-Get-GitRepo "https://github.com/zeromq/cppzmq.git" "cppzmq" "d9f0f016c07046742738c65e1eb84722ae32d7d4"
-Get-GitRepo "https://github.com/zeromq/zmqpp.git" "zmqpp" "f8ff127683dc555aa004c0e6e2b18d2354a375be"
+# Get-GitRepo "https://github.com/zeromq/libzmq.git" "libzmq" "d062edd8c142384792955796329baf1e5a3377cd"
+# Get-GitRepo "https://github.com/zeromq/cppzmq.git" "cppzmq" "d9f0f016c07046742738c65e1eb84722ae32d7d4"
+# Get-GitRepo "https://github.com/zeromq/zmqpp.git" "zmqpp" "f8ff127683dc555aa004c0e6e2b18d2354a375be"
 Get-GitRepo "https://github.com/ThePhD/sol2.git" "sol2" "254466eb4b3ae630c731a557987f3adb1a8f86b0"
 Get-GitRepo "https://github.com/AmokHuginnsson/replxx.git" "replxx" "3cb884e3fb4b1a28efeb716fac75f77eecc7ea3d"
 Get-GitRepo "https://github.com/lua/lua.git" "lua" "e354c6355e7f48e087678ec49e340ca0696725b1"
@@ -60,9 +77,9 @@ Get-GitRepo "https://github.com/weidai11/cryptopp.git" "cryptopp" "c8d8caf700746
 Get-GitRepo "https://github.com/orlp/ed25519.git" "ed25519" "7fa6712ef5d581a6981ec2b08ee623314cd1d1c4"
 Get-GitRepo "https://github.com/google/googletest.git" "googletest" "2fe3bd994b3189899d93f1d5a881e725e046fdc2"
 # Get-GitRepo "https://github.com/nlohmann/json.git" "json" "359f98d14065bf4e53eeb274f5987fd08f16e5bf"
-Get-GitRepo "https://github.com/nelhage/rules_boost.git" "com_github_nelhage_rules_boost" "fe787183c14f2a5c6e5e1e75a7c57d2e799d3d19"
+# Get-GitRepo "https://github.com/nelhage/rules_boost.git" "com_github_nelhage_rules_boost" "fe787183c14f2a5c6e5e1e75a7c57d2e799d3d19"
 Get-GitRepo "https://github.com/protocolbuffers/protobuf.git" "protobuf" "48cb18e5c419ddd23d9badcfe4e9df7bde1979b2"
-Get-GitRepo "https://github.com/svaarala/duktape.git" "duktape" "d7fdb67f18561a50e06bafd196c6b423af9ad6fe"
+# Get-GitRepo "https://github.com/svaarala/duktape.git" "duktape" "d7fdb67f18561a50e06bafd196c6b423af9ad6fe"
 
 #  ====== Check if missing and download using wget ======
 
@@ -70,10 +87,10 @@ if(!(Test-Path -Path .\json-3.1.2) -and (New-Item -ItemType Directory -Path .\js
   wget -URI https://github.com/nlohmann/json/releases/download/v3.2.0/json.hpp -OutFile json-3.1.2/json.hpp
 }
 
-if(!(Test-Path -Path .\boost_1_68_0)) {
-    Get-Archive "https://dl.bintray.com/boostorg/release/1.68.0/source/boost_1_68_0.zip" `
-  "boost_1_68_0.zip" "3B1DB0B67079266C40B98329D85916E910BBADFC3DB3E860C049056788D4D5CD"
-}
+#if(!(Test-Path -Path .\boost_1_68_0)) {
+#    Get-Archive "https://dl.bintray.com/boostorg/release/1.68.0/source/boost_1_68_0.zip" `
+#  "boost_1_68_0.zip" "3B1DB0B67079266C40B98329D85916E910BBADFC3DB3E860C049056788D4D5CD"
+#}
 
 if(!(Test-Path -Path .\zlib-1.2.11)) {
     Get-Archive "https://zlib.net/zlib1211.zip" `
@@ -105,13 +122,13 @@ $lua_extern_includes = "extern `"C`" {
 $lua_extern_includes | Out-File -FilePath .\lua\lua.hpp -Encoding utf8
 
 #  ====== Building LuaJIT ======
-echo ("="*80)
-echo "  BUILDING LuaJIT"
-echo ("="*80)
+# echo ("="*80)
+# echo "  BUILDING LuaJIT"
+# echo ("="*80)
 
-cd LuaJIT\src
-.\msvcbuild.bat
-cd ..\..
+# cd LuaJIT\src
+# .\msvcbuild.bat
+# cd ..\..
 
 #  ====== Building libzmq ======
 echo ("="*80)
@@ -153,14 +170,3 @@ if (!(Test-Path .\CMakeCache.txt)) {
 }
 msbuild.exe replxx.sln /p:configuration=Release
 cd ..\..
-
-#  ====== Building boost ======
-echo ("="*80)
-echo "  BUILDING boost"
-echo ("="*80)
-
-cd boost_1_68_0
-bootstrap.bat
-b2 cxxstd=14 link=static stage
-cd ..
-
