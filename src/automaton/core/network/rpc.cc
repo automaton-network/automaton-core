8dff9ca4e138aa955b3103092528f591098df7bc
Plazmock <empty>
2019-01-03 17:22:32 +0200

    RPC .cc and .h

---
+++ src/automaton/core/network/rpc.cc
@@ -0,0 +1,17 @@
+#include "automaton/core/network/rpc.h"
+
+namespace automaton {
+namespace core {
+namespace network {
+
+
+
+
+
+
+
+
+
+}  // namespace network
+}  // namespace core
+}  // namespace automaton
