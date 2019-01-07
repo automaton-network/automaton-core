a2854544f84c716fbaf0aa83621147f6fae2773d
Asen Kovachev <asenski@gmail.com>
2019-01-07 09:53:41 +0200

    Use curl instead of wget

--- download_and_build_third_party_libs.sh
+++ download_and_build_third_party_libs.sh
@@ -59,7 +59,7 @@ function get_archive() {
   echo "  Downloading $filename from $url"
   print_separator "=" 80
 
-  [ ! -f $2 ] && wget $1 -O $2
+  [ ! -f $2 ] && curl -L $1 -o $2
   filesha=$(shasum -a 256 $filename | cut -d' ' -f1)
   if [ $filesha != $sha ]; then
     echo "Error: Wrong hash [$filesha] Expected [$sha]"
@@ -127,13 +127,13 @@ git_repo "https://github.com/orlp/ed25519.git" "ed25519" "7fa6712ef5d581a6981ec2
 
 [ ! -d json-3.1.2 ] && \
   mkdir json-3.1.2 && \
-  wget https://github.com/nlohmann/json/releases/download/v3.2.0/json.hpp -O json-3.1.2/json.hpp
+  curl -L https://github.com/nlohmann/json/releases/download/v3.2.0/json.hpp -o json-3.1.2/json.hpp
 
 [ ! -d sol2 ] && \
   mkdir sol2 && \
   mkdir sol2/single && \
   mkdir sol2/single/sol && \
-  wget https://github.com/ThePhD/sol2/releases/download/v2.20.6/sol.hpp -O sol2/single/sol/sol.hpp
+  curl -L https://github.com/ThePhD/sol2/releases/download/v2.20.6/sol.hpp -o sol2/single/sol/sol.hpp
 
 [ ! -d boost_1_68_0 ] && \
   get_archive "https://dl.bintray.com/boostorg/release/1.68.0/source/boost_1_68_0.tar.gz" \
