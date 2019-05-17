8b2d13b1af8eef2162d305deb639f2a83743060e
Asen Kovachev <akovachev@users.noreply.github.com>
2019-05-17 11:22:38 +0300

    Update build-and-run-automaton-core to use CMake (#62)

--- build-and-run-automaton-core.sh
+++ build-and-run-automaton-core.sh
@@ -1,5 +1,12 @@
 #/bin/bash
+CPUCOUNT=$(grep -c "^processor" /proc/cpuinfo)
+
 cd src
 mkdir -p logs
-bazel build -c opt //automaton/core && bazel-bin/automaton/core/core # 2>/dev/null
+mkdir -p build
+cd build
+cmake -DCMAKE_BUILD_TYPE=Release .. && make -j$CPUCOUNT
+cd ..
+build/automaton-core-cli
+# bazel build -c opt //automaton/core && bazel-bin/automaton/core/core # 2>/dev/null
 #reset
