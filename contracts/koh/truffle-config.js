f62e793a65e50e8012a039421163df4c7ec8447f
Asen Kovachev <akovachev@users.noreply.github.com>
2019-05-31 23:26:56 +0300

    Add ganache as a network to truffle config (#78)

--- contracts/koh/truffle-config.js
+++ contracts/koh/truffle-config.js
@@ -75,6 +75,11 @@ module.exports = {
       // network_id: 2111,   // This network is yours, in the cloud.
       // production: true    // Treats this network as if it was a public net. (default: false)
     // }
+    ganache: {
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "*"
+    }
   },
 
   // Set default mocha options here, use special reporters etc.
