5d982b810a0744c200be8eaf293eea2b3c832477
Asen Kovachev <asenski@gmail.com>
2019-09-08 22:48:32 +0300

    extract slots data in bulk from smart contract

--- contracts/koh/migrations/2_deploy_contracts.js
+++ contracts/koh/migrations/2_deploy_contracts.js
@@ -1,7 +1,7 @@
 var KingAutomaton = artifacts.require("./KingAutomaton.sol");
 
 module.exports = function(deployer) {
-  numSlots = 16;
+  numSlots = 65536;
   difficultyBits = 16;
   mask = "53272589901149737477561849970166322707710816978043543010898806474236585144509";
   initialDailySupply ="406080000";
