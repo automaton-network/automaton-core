f218ecd9f4bdaa06402eaf57c1000dba121d766a
Carrie <karonka11@gmail.com>
2019-09-23 11:07:50 +0300

    Ethereum transaction class

---
+++ src/automaton/core/interop/ethereum/eth_transaction.cc
@@ -0,0 +1,130 @@
+#include "automaton/core/interop/ethereum/eth_transaction.h"
+
+#include <iostream>  // for debugging purposes / to be removed
+
+#include "secp256k1/include/secp256k1_recovery.h"
+#include "secp256k1/include/secp256k1.h"
+#include "secp256k1/src/hash_impl.h"
+#include "secp256k1/src/hash.h"
+
+#include "automaton/core/crypto/cryptopp/Keccak_256_cryptopp.h"
+#include "automaton/core/io/io.h"
+#include "automaton/tools/miner/miner.h"
+
+using automaton::core::crypto::cryptopp::Keccak_256_cryptopp;
+using automaton::core::interop::ethereum::hash;
+using automaton::core::io::bin2hex;
+using automaton::core::io::dec2hex;
+using automaton::core::io::hex2bin;
+using automaton::core::io::hex2dec;
+using automaton::tools::miner::sign;
+
+namespace automaton {
+namespace core {
+namespace interop {
+namespace ethereum {
+
+std::string hash(const std::string& data) {
+  Keccak_256_cryptopp hasher;
+  uint8_t digest[32];
+  hasher.calculate_digest(reinterpret_cast<const uint8_t*>(data.data()), data.size(), digest);
+  return std::string(reinterpret_cast<char*>(digest), 32);
+}
+
+std::string rlp_encode(std::string s, bool is_list) {
+  if (s.size() % 2) {
+    s = "0" + s;
+  }
+  uint32_t length = s.size() / 2;
+  if (length == 0) {
+    return "80";
+  }
+  if (length == 1 && hex2dec(s.substr(0, 2)) < 128) {
+    return s;
+  }
+  std::stringstream ss;
+  if (length < 56) {
+    ss << dec2hex((is_list ? 192 : 128) + length) << s;
+  } else {
+    std::string length_in_hex = dec2hex(length);
+    uint32_t first_byte = (is_list ? 247 : 183) + (length_in_hex.size() / 2);
+    // todo: check if this byte is valid
+    ss << dec2hex(first_byte) << length_in_hex << s;
+  }
+  return ss.str();
+}
+
+std::string check_and_sign(const unsigned char* priv_key, const unsigned char* message) {
+  secp256k1_context* context = secp256k1_context_create(SECP256K1_CONTEXT_SIGN | SECP256K1_CONTEXT_VERIFY);
+  secp256k1_pubkey* pubkey = new secp256k1_pubkey();
+
+  std::string rsv = sign(priv_key, message);
+
+  if (!secp256k1_ec_pubkey_create(context, pubkey, priv_key)) {
+    LOG(WARNING) << "Invalid private key!!!" << bin2hex(std::string(reinterpret_cast<const char*>(priv_key), 32));
+    return "";
+  }
+  return rsv;
+}
+
+std::string recover_addres(const unsigned char* rsv, const unsigned char* message) {
+  int32_t v = rsv[64];
+  v -= 27;
+  auto* ctx = secp256k1_context_create(SECP256K1_CONTEXT_SIGN | SECP256K1_CONTEXT_VERIFY);
+  secp256k1_ecdsa_recoverable_signature signature;
+  if (!secp256k1_ecdsa_recoverable_signature_parse_compact(ctx, &signature, (unsigned char*)rsv, v)) {
+    LOG(ERROR) << "Cannot parse signature!";
+    return "";
+  }
+  secp256k1_pubkey* pubkey = new secp256k1_pubkey();
+  if (!secp256k1_ecdsa_recover(ctx, pubkey, &signature, (unsigned char*) message)) {
+    LOG(ERROR) << "Cannot recover signature!";
+    return "";
+  }
+
+  size_t out_len = 65;
+  unsigned char pub_key_serialized[out_len];
+  secp256k1_ec_pubkey_serialize(ctx, pub_key_serialized, &out_len, pubkey, SECP256K1_EC_UNCOMPRESSED);
+  std::string pub_key_uncompressed(reinterpret_cast<char*>(pub_key_serialized), out_len);
+  return hash(pub_key_uncompressed.substr(1)).substr(12);
+}
+
+std::string eth_transaction::sign_tx(const std::string& private_key_hex) {
+  std::string rlp_tx = to_rlp_encoded_tx();
+  std::string extended_sign_tx = rlp_encode(rlp_tx + rlp_encode(chainId, false) + "8080", true);
+  std::string byte_array = hex2bin(extended_sign_tx);
+  // std::cout << "BEFORE HASH tx hex: " << extended_sign_tx << std::endl;
+  std::string hashed_tx = hash(byte_array);
+  // std::cout << "HASH tx hex: " << bin2hex(hashed_tx) << std::endl;
+  std::string pr_key = hex2bin(private_key_hex);
+  std::string rsv = check_and_sign(reinterpret_cast<const unsigned char*>(pr_key.c_str()),
+      reinterpret_cast<const unsigned char*>(hashed_tx.c_str()));
+  int32_t v = rsv[64];
+  v -= 27;
+  // std::cout << "ADDRESS: " << bin2hex(recover_addres(reinterpret_cast<const unsigned char*>(rsv.c_str()),
+      // reinterpret_cast<const unsigned char*>(hashed_tx.c_str()))) << std::endl;
+  std::stringstream tx;
+  uint32_t newv = hex2dec(chainId) * 2 + ((v % 2) ? 36 : 35);
+  tx << rlp_tx << rlp_encode(dec2hex(newv), false) << "a0" << bin2hex(rsv.substr(0, 32)) <<
+      "a0" << bin2hex(rsv.substr(32, 32));
+  // std::cout << "\nSIGNED TX:\n" << tx.str() << std::endl;
+  return rlp_encode(tx.str(), true);
+}
+
+std::string eth_transaction::to_rlp_encoded_tx() {
+  std::stringstream tx;
+  tx << rlp_encode(nonce, false) <<
+      rlp_encode(gasPrice, false) <<
+      rlp_encode(gasLimit, false) <<
+      rlp_encode(to, false) <<
+      rlp_encode(value, false) <<
+      rlp_encode(data, false);
+  return tx.str();
+}
+
+
+
+}  // namespace ethereum
+}  // namespace interop
+}  // namespace core
+}  // namespace automaton
---
+++ src/automaton/core/interop/ethereum/eth_transaction.h
@@ -0,0 +1,45 @@
+#ifndef AUTOMATON_CORE_INTEROP_ETHEREUM_ETH_TRANSACTION_H_
+#define AUTOMATON_CORE_INTEROP_ETHEREUM_ETH_TRANSACTION_H_
+
+#include <string>
+
+namespace automaton {
+namespace core {
+namespace interop {
+namespace ethereum {
+
+std::string hash(const std::string& data);
+
+std::string rlp_encode(std::string s, bool is_list);
+
+std::string check_and_sign(const unsigned char* priv_key, const unsigned char* message);
+
+std::string recover_addres(const unsigned char* rsv, const unsigned char* message);
+
+/**
+  Ethereum transaction. Transaction fields should be given as hex strings WITHOUT '0x' prefix.
+*/
+class eth_transaction {
+ public:
+  std::string nonce;
+  std::string gasPrice;
+  std::string gasLimit;
+  std::string to;
+  std::string value;
+  std::string data;
+  std::string chainId;
+
+  std::string sign_tx(const std::string& private_key_hex);
+
+ private:
+  std::string to_rlp_encoded_tx();
+};
+
+
+}  // namespace ethereum
+}  // namespace interop
+}  // namespace core
+}  // namespace automaton
+
+
+#endif  // AUTOMATON_CORE_INTEROP_ETHEREUM_ETH_TRANSACTION_H_
