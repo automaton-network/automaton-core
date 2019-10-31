5c9de16015ed0949b9725e68d0ac7429be10c41e
Carrie <karonka11@gmail.com>
2019-10-31 13:19:51 +0200

    test contract + refactor

---
+++ contracts/test_contract/contracts/test_contract.sol
@@ -0,0 +1,179 @@
+pragma solidity ^0.5.11;
+pragma experimental ABIEncoderV2;
+
+contract test_contract {
+  constructor() public {
+    alabala = 34;
+  }
+
+  int256 alabala;
+
+  function f1(uint32 a) public pure returns (uint256) {
+    return a + 256;
+  }
+
+  function f2(uint8 a) public pure returns (uint256[] memory result) {
+    result = new uint[](a);
+    for(uint256 i = 0; i < a; i++) {
+      result[i] = i*i;
+    }
+  }
+
+  function f3(int40 a, int32 b) public pure returns (int32 c, int40 d) {
+    c = b;
+    d = a;
+  }
+
+  function f4(string memory a) public pure returns (string memory result) {
+    bytes memory a_bytes = bytes(a);
+    bytes memory res = new bytes(a_bytes.length);
+    for(uint16 i = 0; i < a_bytes.length; i++) {
+      res[i] = a_bytes[a_bytes.length - i - 1];
+    }
+    result = string(res);
+  }
+
+  function f5() public pure returns (bytes5 result) {
+    result = 0xABCDEABCDE;
+  }
+
+  function f6() public view returns (address) {
+    return address(this);
+  }
+
+  function f7(bool a) public pure returns (bool) {
+    return !a;
+  }
+
+  function f8(int a) public pure returns (int) {
+    return -a;
+  }
+
+  function f9(int8 a) public pure returns (int16) {
+    return 2 * a;
+  }
+
+  function f10(uint16[2][3] memory a) public pure returns (uint b) {
+    b = 1;
+    for (uint16 i = 0; i < 3; ++i) {
+      for (uint16 j = 0; j < 2; ++j) {
+        b *= a[i][j];
+      }
+    }
+  }
+
+  function f11(uint16[][] memory a) public pure returns (uint b) {
+    b = 1;
+    for (uint16 i = 0; i < a.length; ++i) {
+      for (uint16 j = 0; j < a[i].length; ++j) {
+        b *= a[i][j];
+      }
+    }
+  }
+
+  function f12(uint[5] memory a) public pure returns (uint b) {
+    b = 0;
+    for (uint16 i = 0; i < 5; ++i) {
+      b += a[i];
+    }
+  }
+
+  function f13() public pure returns (uint[][] memory a) {
+    a = new uint[][](2);
+    for (uint16 i = 0; i < 2; ++i) {
+      a[i] = new uint[](3);
+      for (uint16 j = 0; j < 3; ++j) {
+        a[i][j] = i + j;
+      }
+    }
+  }
+
+  function f14() public pure returns (uint[3][2] memory a) {
+    for (uint16 i = 0; i < 2; ++i) {
+      for (uint16 j = 0; j < 3; ++j) {
+        a[i][j] = i + j;
+      }
+    }
+  }
+
+  function f15() public pure returns (uint[3][] memory a) {
+    a = new uint[3][](2);
+    for (uint16 i = 0; i < 2; ++i) {
+      for (uint16 j = 0; j < 3; ++j) {
+        a[i][j] = i + j;
+      }
+    }
+  }
+
+  function f16() public pure returns (uint[][2] memory a) {
+    for (uint16 i = 0; i < 2; ++i) {
+      a[i] = new uint[](3);
+      for (uint16 j = 0; j < 3; ++j) {
+        a[i][j] = i + j;
+      }
+    }
+  }
+
+  function f17(uint16[3][] memory a) public pure returns (uint16[][3] memory b) {
+    for (uint16 i = 0; i < 3; ++i) {
+      b[i] = new uint16[](2);
+      for (uint16 j = 0; j < 2; ++j) {
+        b[i][j] = a[j][i];
+      }
+    }
+  }
+
+  function f18(uint16[][3] memory a) public pure returns (uint16[3][] memory b) {
+    b = new uint16[3][](2);
+    for (uint16 i = 0; i < 2; ++i) {
+      for (uint16 j = 0; j < 3; ++j) {
+        b[i][j] = a[j][i];
+      }
+    }
+  }
+
+  function f19(string[3][] memory a) public pure returns (string[][3] memory b) {
+    for (uint16 i = 0; i < 3; ++i) {
+      b[i] = new string[](2);
+      for (uint16 j = 0; j < 2; ++j) {
+        b[i][j] = a[j][i];
+      }
+    }
+  }
+
+  function f20(string[][3] memory a) public pure returns (string[3][] memory b) {
+    b = new string[3][](2);
+    for (uint16 i = 0; i < 2; ++i) {
+      for (uint16 j = 0; j < 3; ++j) {
+        b[i][j] = a[j][i];
+      }
+    }
+  }
+
+  function f21(uint256[4][2] memory a, string[][2][2] memory b) public pure returns
+      (uint[4][1][2] memory c, bool d, string[][4] memory e) {
+    for (uint16 i = 0; i < 4; ++i) {
+      c[0][0][i] = a[1][i];
+      c[1][0][i] = a[0][i];
+    }
+
+    d = true;
+
+    e[0] = b[0][0];
+    e[1] = b[0][1];
+    e[2] = b[1][0];
+    e[3] = b[1][1];
+  }
+
+  function f22(string[][2][] memory a) public pure returns (string[][2][] memory b) {
+    // a -> [1][2][3], b -> [3][2][1]
+    b = new string[][2][](1);
+    b[0][0] = new string[](3);
+    b[0][1] = new string[](3);
+    for (uint16 i = 0; i < 3; ++i) {
+      b[0][0][i] = a[i][0][0];
+      b[0][1][i] = a[i][1][0];
+    }
+  }
+
+}
--- src/automaton/tests/interop/abi_encoder_test.cc
+++ src/automaton/tests/interop/abi_encoder_test.cc
@@ -12,17 +12,8 @@
 
 using json = nlohmann::json;
 
-using automaton::core::interop::ethereum::calculate_offset;
-using automaton::core::interop::ethereum::decode;
-using automaton::core::interop::ethereum::encode;
-using automaton::core::interop::ethereum::extract_array_type;
-using automaton::core::interop::ethereum::get_type;
-using automaton::core::interop::ethereum::numerical;
-using automaton::core::interop::ethereum::fixed_size_bytes;
-using automaton::core::interop::ethereum::fixed_size_array;
-using automaton::core::interop::ethereum::dynamic_size_bytes;
-using automaton::core::interop::ethereum::dynamic_size_array;
-using automaton::core::interop::ethereum::string;
+using namespace automaton::core::interop::ethereum;  // NOLINT
+
 using automaton::core::io::bin2hex;
 using automaton::core::io::hex2bin;
 using automaton::core::io::hex2dec;
@@ -35,55 +26,87 @@ int main() {
   auto t = get_type("int");
   EXPECT_EQ(t.str, "int");
   EXPECT_EQ(t.s_type, numerical);
+  EXPECT_EQ(t.s_array_type, no_array);
   EXPECT_EQ(t.array_len, 0);
   t = get_type("int[][]");
   EXPECT_EQ(t.str, "int[][]");
-  EXPECT_EQ(t.s_type, numerical | dynamic_size_array);
+  EXPECT_EQ(t.s_type, numerical);
+  EXPECT_EQ(t.s_array_type, dynamic);
   EXPECT_EQ(t.array_len, 0);
   t = get_type("fixed[5]");
   EXPECT_EQ(t.str, "fixed[5]");
-  EXPECT_EQ(t.s_type, numerical | fixed_size_array);
+  EXPECT_EQ(t.s_type, numerical);
+  EXPECT_EQ(t.s_array_type, fixed);
   EXPECT_EQ(t.array_len, 5);
   t = get_type("bytes32");
   EXPECT_EQ(t.str, "bytes32");
   EXPECT_EQ(t.s_type, fixed_size_bytes);
+  EXPECT_EQ(t.s_array_type, no_array);
   EXPECT_EQ(t.array_len, 0);
   t = get_type("string");
   EXPECT_EQ(t.str, "string");
   EXPECT_EQ(t.s_type, string);
+  EXPECT_EQ(t.s_array_type, no_array);
   EXPECT_EQ(t.array_len, 0);
   t = get_type("string[5][]");
   EXPECT_EQ(t.str, "string[5][]");
-  EXPECT_EQ(t.s_type, string | fixed_size_array);
+  EXPECT_EQ(t.s_type, string);
+  EXPECT_EQ(t.s_array_type, dynamic);
+  EXPECT_EQ(t.array_len, 0);
+  t = get_type("string[][5]");
+  EXPECT_EQ(t.str, "string[][5]");
+  EXPECT_EQ(t.s_type, string);
+  EXPECT_EQ(t.s_array_type, dynamic);
+  EXPECT_EQ(t.array_len, 5);
+  t = get_type("string[4][5]");
+  EXPECT_EQ(t.str, "string[4][5]");
+  EXPECT_EQ(t.s_type, string);
+  EXPECT_EQ(t.s_array_type, dynamic);
+  EXPECT_EQ(t.array_len, 5);
+  t = get_type("int[4][5]");
+  EXPECT_EQ(t.str, "int[4][5]");
+  EXPECT_EQ(t.s_type, numerical);
+  EXPECT_EQ(t.s_array_type, fixed);
   EXPECT_EQ(t.array_len, 5);
 
   t = extract_array_type("uint256[][]");
   EXPECT_EQ(t.str, "uint256[]");
-  EXPECT_EQ(t.s_type, numerical | dynamic_size_array);
+  EXPECT_EQ(t.s_type, numerical);
+  EXPECT_EQ(t.s_array_type, dynamic);
   EXPECT_EQ(t.array_len, 0);
 
   t = extract_array_type("string[5][]");
-  EXPECT_EQ(t.str, "string[]");
-  EXPECT_EQ(t.s_type, string | dynamic_size_array);
-  EXPECT_EQ(t.array_len, 0);
+  EXPECT_EQ(t.str, "string[5]");
+  EXPECT_EQ(t.s_type, string);
+  EXPECT_EQ(t.s_array_type, dynamic);
+  EXPECT_EQ(t.array_len, 5);
 
   t = extract_array_type("int[][5]");
+  EXPECT_EQ(t.str, "int[]");
+  EXPECT_EQ(t.s_type, numerical);
+  EXPECT_EQ(t.s_array_type, dynamic);
+  EXPECT_EQ(t.array_len, 0);
+
+  t = extract_array_type("int[5][]");
   EXPECT_EQ(t.str, "int[5]");
-  EXPECT_EQ(t.s_type, numerical | fixed_size_array);
+  EXPECT_EQ(t.s_type, numerical);
+  EXPECT_EQ(t.s_array_type, fixed);
   EXPECT_EQ(t.array_len, 5);
 
   t = extract_array_type("bytes[]");
   EXPECT_EQ(t.str, "bytes");
   EXPECT_EQ(t.s_type, dynamic_size_bytes);
+  EXPECT_EQ(t.s_array_type, no_array);
   EXPECT_EQ(t.array_len, 0);
 
   EXPECT_EQ(calculate_offset({"uint256"}), 32);
-  EXPECT_EQ(calculate_offset({"uint8", "string", "string[5][]", "bool"}), 8 * 32);
+  EXPECT_EQ(calculate_offset({"uint8", "string", "string[][5]", "bool"}), 4 * 32);
   EXPECT_EQ(calculate_offset({"fixed[2][3][4]"}), 24 * 32);
   EXPECT_EQ(calculate_offset({"string", "bytes"}), 64);
   EXPECT_EQ(calculate_offset({"string[]", "uint32"}), 64);
-  EXPECT_EQ(calculate_offset({"bool[3]", "uint256[7][]"}), 320);
+  EXPECT_EQ(calculate_offset({"bool[3]", "uint256[7][]"}), 4 * 32);
   EXPECT_EQ(calculate_offset({"uint256[][10]"}), 32);
+  EXPECT_EQ(calculate_offset({"uint256[10]"}), 320);
 
   std::string encoded, decoded, signatures, parameters;
 
@@ -153,25 +176,25 @@ int main() {
   decoded = decode(signatures, encoded);
   EXPECT_EQ(decoded, "[[\"1\",\"2\"]]");
 
-  signatures = "[\"uint256[2][3]\"]";
+  signatures = "[\"uint256[3][2]\"]";
   parameters = "[[[\"1\",\"2\",\"3\"],[5,6,7]]]";
   encoded = encode(signatures, parameters);
   decoded = decode(signatures, encoded);
   EXPECT_EQ(decoded, "[[[\"1\",\"2\",\"3\"],[\"5\",\"6\",\"7\"]]]");
 
-  signatures = "[\"uint256[2][3]\", \"string[][2][]\"]";
+  signatures = "[\"uint256[3][2]\", \"string[][2][]\"]";
   parameters = "[[[\"1\",\"2\",\"3\"],[\"5\",\"6\",\"7\"]],[[[\"aaa\"],[\"eee\"]],[[\"ooo\"],[\"ppp\",\"ooo\"]]]]";
   encoded = encode(signatures, parameters);
   decoded = decode(signatures, encoded);
   EXPECT_EQ(decoded, parameters);
 
-  signatures = "[\"string[][2][]\", \"uint256[2][3]\"]";
+  signatures = "[\"string[][2][]\", \"uint256[3][2]\"]";
   parameters = "[[[[\"aaa\"],[\"eee\"]],[[\"ooo\"],[\"ppp\",\"rrr\"]]],[[\"1\",\"2\",\"3\"],[\"5\",\"6\",\"7\"]]]";
   encoded = encode(signatures, parameters);
   decoded = decode(signatures, encoded);
   EXPECT_EQ(decoded, parameters);
 
-  signatures = "[\"uint256[2][3]\", \"string[][2][2]\"]";
+  signatures = "[\"uint256[3][2]\", \"string[2][2][]\"]";
   parameters = "[[[\"1\",\"2\",\"3\"],[\"5\",\"6\",\"7\"]],[[[\"aaa\",\"eee\"],[\"ppp\",\"rrr\"]]]]";
   encoded = encode(signatures, parameters);
   decoded = decode(signatures, encoded);
@@ -189,5 +212,115 @@ int main() {
   decoded = decode(signatures, encoded);
   EXPECT_EQ(decoded, parameters);
 
+  // Test contract functions
+
+  const char* JSON_FILE = "../contracts/test_contract/build/contracts/test_contract.json";
+  const char* URL = "127.0.0.1:7545";
+  const char* CONTRACT_ADDR = "0x22D9d6faB361FaA969D2EfDE420472633cBB7B11";
+  const char* ADDRESS = "0x603CB0d1c8ab86E72beb3c7DF564A36D7B85ecD2";
+  const char* PRIVATE_KEY = "56aac550d97013a8402c98e3b2aeb20482d19f142a67022d2ab357eb8bb673b0";
+
+  curl_global_init(CURL_GLOBAL_ALL);
+
+  using namespace automaton::core::interop::ethereum;  // NOLINT
+
+  std::fstream fs(JSON_FILE, std::fstream::in);
+  json j, abi;
+  if (!fs.is_open()) {
+    LOG(FATAL) << "No contract json file!";
+  }
+  fs >> j;
+  if (j.find("abi") != j.end()) {
+    abi = j["abi"].get<json>();
+  } else {
+    LOG(FATAL) << "No abi!";
+  }
+  std::string abi_string = abi.dump();
+  eth_contract::register_contract(URL, CONTRACT_ADDR, abi_string);
+  fs.close();
+
+  auto contract = eth_contract::get_contract(CONTRACT_ADDR);
+  if (contract == nullptr) {
+    LOG(ERROR) << "Contract is NULL";
+    curl_global_cleanup();
+    return 0;
+  }
+
+  status s = status::ok();
+
+  s = contract->call("f1", "[3]");
+  EXPECT_EQ(s.msg, "[\"259\"]");
+
+  s = contract->call("f2", "[3]");
+  EXPECT_EQ(s.msg, "[[\"0\",\"1\",\"4\"]]");
+
+  s = contract->call("f3", "[5,7]");
+  EXPECT_EQ(s.msg, "[\"7\",\"5\"]");
+
+  s = contract->call("f4", "[\"abc\"]");
+  EXPECT_EQ(s.msg, "[\"cba\"]");
+
+  s = contract->call("f5", "");
+  EXPECT_EQ(s.msg, "[\"ABCDEABCDE\"]");
+
+  s = contract->call("f6", "");
+  EXPECT_EQ(s.msg, "[\"22D9D6FAB361FAA969D2EFDE420472633CBB7B11\"]");
+
+  s = contract->call("f7", "[true]");
+  EXPECT_EQ(s.msg, "[false]");
+  s = contract->call("f7", "[false]");
+  EXPECT_EQ(s.msg, "[true]");
+
+  s = contract->call("f8", "[5]");
+  EXPECT_EQ(s.msg, "[\"-5\"]");
+  s = contract->call("f8", "[0]");
+  EXPECT_EQ(s.msg, "[\"0\"]");
+  s = contract->call("f8", "[\"18446744073709551615\"]");
+  EXPECT_EQ(s.msg, "[\"-18446744073709551615\"]");
+  s = contract->call("f8", "[\"518446744073709551615\"]");
+  EXPECT_EQ(s.msg, "[\"-518446744073709551615\"]");
+
+  s = contract->call("f9", "[5]");
+  EXPECT_EQ(s.msg, "[\"10\"]");
+
+  s = contract->call("f10", "[[[2,3],[5,7],[11,13]]]");
+  EXPECT_EQ(s.msg, "[\"30030\"]");
+
+  s = contract->call("f11", "[[[2,3,5],[7,11,13]]]");
+  EXPECT_EQ(s.msg, "[\"30030\"]");
+
+  s = contract->call("f12", "[[1,2,3,4,5]]");
+  EXPECT_EQ(s.msg, "[\"15\"]");
+
+  s = contract->call("f13", "");
+  EXPECT_EQ(s.msg, "[[[\"0\",\"1\",\"2\"],[\"1\",\"2\",\"3\"]]]");
+
+  s = contract->call("f14", "");
+  EXPECT_EQ(s.msg, "[[[\"0\",\"1\",\"2\"],[\"1\",\"2\",\"3\"]]]");
+
+  s = contract->call("f15", "");
+  EXPECT_EQ(s.msg, "[[[\"0\",\"1\",\"2\"],[\"1\",\"2\",\"3\"]]]");
+
+  s = contract->call("f16", "");
+  EXPECT_EQ(s.msg, "[[[\"0\",\"1\",\"2\"],[\"1\",\"2\",\"3\"]]]");
+
+  s = contract->call("f17", "[[[\"0\",\"1\",\"2\"],[\"1\",\"2\",\"3\"]]]");
+  EXPECT_EQ(s.msg, "[[[\"0\",\"1\"],[\"1\",\"2\"],[\"2\",\"3\"]]]");
+
+  s = contract->call("f18", "[[[\"0\",\"1\"],[\"1\",\"2\"],[\"2\",\"3\"]]]");
+  EXPECT_EQ(s.msg, "[[[\"0\",\"1\",\"2\"],[\"1\",\"2\",\"3\"]]]");
+
+  s = contract->call("f19", "[[[\"aa\",\"bb\",\"cc\"],[\"ss\",\"pp\",\"rr\"]]]");
+  EXPECT_EQ(s.msg, "[[[\"aa\",\"ss\"],[\"bb\",\"pp\"],[\"cc\",\"rr\"]]]");
+
+  s = contract->call("f20", "[[[\"aa\",\"ss\"],[\"bb\",\"pp\"],[\"cc\",\"rr\"]]]");
+  EXPECT_EQ(s.msg, "[[[\"aa\",\"bb\",\"cc\"],[\"ss\",\"pp\",\"rr\"]]]");
+
+  s = contract->call("f21",
+  "[[[\"0\",\"1\",\"2\",\"3\"],[\"4\",\"5\",\"6\",\"7\"]],[[[\"a\",\"ab\"],[\"abc\"]],[[\"b\",\"ba\"],[\"cb\",\"ca\",\"c\"]]]]]");  // NOLINT
+  EXPECT_EQ(s.msg, "[[[[\"4\",\"5\",\"6\",\"7\"]],[[\"0\",\"1\",\"2\",\"3\"]]],true,[[\"a\",\"ab\"],[\"abc\"],[\"b\",\"ba\"],[\"cb\",\"ca\",\"c\"]]]");  // NOLINT
+
+  s = contract->call("f22", "[[[[\"aa\"],[\"ss\"]],[[\"bb\"],[\"pp\"]],[[\"cc\"],[\"rr\"]]]]");
+  EXPECT_EQ(s.msg, "[[[[\"aa\",\"bb\",\"cc\"],[\"ss\",\"pp\",\"rr\"]]]]");
   return 0;
 }
