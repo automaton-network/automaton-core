5c8486dcb7a71ffe3668b0b349d2c706737a64fc
Asen Kovachev <akovachev@users.noreply.github.com>
2019-12-02 16:32:56 +0200

    Update download_and_build_JUCE.sh

--- scripts/download_and_build_JUCE.sh
+++ scripts/download_and_build_JUCE.sh
@@ -3,7 +3,7 @@ JUCE_VER="5.4.5"
 # Download JUCE
 [ ! -d JUCE ] && \
   get_archive "https://github.com/WeAreROLI/JUCE/archive/$JUCE_VER.tar.gz" \
-  "JUCE-$JUCE_VER.tar.gz" "40536c5792002ceec09faea8d5a57d5c17486f8805c5efe3bc734e27d2df2202"
+  "JUCE-$JUCE_VER.tar.gz" "e51019065f1185db124954959aeb9101d759a4f2a4dd0c4a6b305a37f0c9271f"
 [ -d JUCE-$JUCE_VER ] && mv JUCE-$JUCE_VER JUCE
 
 # Build JUCE Projucer -- only when not in CI
