83586c5fa0a6805ff58774b18a64d814de96bd7e
Carrie <karonka11@gmail.com>
2020-02-18 09:54:55 +0200

    small contract refactor

--- contracts/koh/contracts/KingAutomaton.sol
+++ contracts/koh/contracts/KingAutomaton.sol
@@ -1,18 +1,25 @@
 pragma solidity ^0.6.2;
 
 contract KingAutomaton {
+
+  int256 constant INT256_MIN = int256(uint256(1) << 255);
+  int256 constant INT256_MAX = int256(~(uint256(1) << 255));
+  uint256 constant UINT256_MIN = 0;
+  uint256 constant UINT256_MAX = ~uint256(0);
+  uint256 constant MSB_SET = 1 << 255;
+  uint256 constant ALL_BUT_MSB = MSB_SET - 1;
+
+  bool public debugging = false;
+
   //////////////////////////////////////////////////////////////////////////////////////////////////
   // Initialization
   //////////////////////////////////////////////////////////////////////////////////////////////////
 
-  // TODO(kari): write withdraw() function to replace .transfer
-
-  constructor(uint256 nSlots, uint8 minDifficultyBits, uint256 predefinedMask, uint256 initialDailySupply,
-    int256 approval_pct, int256 contest_pct, uint256 treasury_limit_pct) public {
+  constructor(uint256 nSlots, uint256 minDifficultyBits, uint256 predefinedMask, uint256 initialDailySupply,
+      int256 approval_pct, int256 contest_pct, uint256 treasury_limit_pct) public {
     numSlots = nSlots;
     initMining(nSlots, minDifficultyBits, predefinedMask, initialDailySupply);
     initNames();
-    initTreasury();
 
     require(approval_pct > contest_pct, "Approval percentage must be bigger than contest percentage!");
     approvalPercentage = approval_pct;
@@ -45,12 +52,11 @@ contract KingAutomaton {
   event Transfer(address indexed _from, address indexed _to, uint256 _value);
   event Approval(address indexed _owner, address indexed _spender, uint256 _value);
 
-  uint256 constant private MAX_uint = 2**256 - 1;
   mapping (address => uint256) public balances;
   mapping (address => mapping (address => uint256)) public allowed;
 
   function transfer(address _to, uint256 _value) public returns (bool success) {
-    require(balances[msg.sender] >= _value);
+    require(balances[msg.sender] >= _value && balances[_to] + _value >= balances[_to]);
     balances[msg.sender] -= _value;
     balances[_to] += _value;
     emit Transfer(msg.sender, _to, _value); //solhint-disable-line indent, no-unused-vars
@@ -59,10 +65,10 @@ contract KingAutomaton {
 
   function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
     uint256 allowance = allowed[_from][msg.sender];
-    require(balances[_from] >= _value && allowance >= _value);
+    require(balances[_from] >= _value && allowance >= _value && balances[_to] + _value >= balances[_to]);
     balances[_to] += _value;
     balances[_from] -= _value;
-    if (allowance < MAX_uint) {
+    if (allowance < UINT256_MAX) {
         allowed[_from][msg.sender] -= _value;
     }
     emit Transfer(_from, _to, _value); //solhint-disable-line indent, no-unused-vars
@@ -87,6 +93,7 @@ contract KingAutomaton {
   // Those accounts help to represent the total supply correctly.
   function transferInternal(address _from, address _to, uint256 _value) private returns (bool success) {
     require(balances[_from] >= _value, "Insufficient balance");
+    require(balances[_to] + _value > balances[_to]);
     balances[_to] += _value;
     balances[_from] -= _value;
     emit Transfer(_from, _to, _value); //solhint-disable-line indent, no-unused-vars
@@ -94,6 +101,7 @@ contract KingAutomaton {
   }
 
   function mint(address _receiver, uint256 _value) private {
+    require(balances[_receiver] + _value > balances[_receiver] && totalSupply + _value > totalSupply);
     balances[_receiver] += _value;
     totalSupply += _value;
     emit Transfer(address(0), _receiver, _value); //solhint-disable-line indent, no-unused-vars
@@ -102,13 +110,6 @@ contract KingAutomaton {
   //////////////////////////////////////////////////////////////////////////////////////////////////
   // Treasury
   //////////////////////////////////////////////////////////////////////////////////////////////////
-
-  int256 constant INT256_MIN = int256(uint256(1) << 255);
-  int256 constant INT256_MAX = int256(~(uint256(1) << 255));
-  uint256 constant UINT256_MIN = 0;
-  uint256 constant UINT256_MAX = ~uint256(0);
-  uint256 constant MSB_SET = 1 << 255;
-  uint256 constant ALL_BUT_MSB = MSB_SET - 1;
 
   uint256 public constant PROPOSAL_START_PERIOD = 90 seconds; // 1 weeks;
   uint256 public constant CONTEST_PERIOD = 90 seconds;  //
@@ -119,23 +120,8 @@ contract KingAutomaton {
 
   uint256 private ballotBoxIDs = 99;  // Ensure special addresses are not already used
 
-  function msb(uint256 x) public pure returns (uint8 r) {
-    if (x >= 0x100000000000000000000000000000000) {x >>= 128; r += 128;}
-    if (x >= 0x10000000000000000) {x >>= 64; r += 64;}
-    if (x >= 0x100000000) {x >>= 32; r += 32;}
-    if (x >= 0x10000) {x >>= 16; r += 16;}
-    if (x >= 0x100) {x >>= 8; r += 8;}
-    if (x >= 0x10) {x >>= 4; r += 4;}
-    if (x >= 0x4) {x >>= 2; r += 2;}
-    if (x >= 0x2) r += 1; // No need to shift x anymore
-  }
-
   enum BallotBoxState {Uninitialized, PrepayingGas, Active, Inactive}
   enum ProposalState {Uninitialized, Started, Accepted, Rejected, Contested, Completed}
-
-  // TODO(kari): Check warnings !!!
-
-  // event ProposalCreated(address from, uint256 id);
 
   /*
   ##################################
@@ -187,8 +173,6 @@ contract KingAutomaton {
     uint256 contestEndDate;
   }
 
-  bool public debugging = false;
-
   mapping (uint256 => BallotBox) public ballotBoxes;
   mapping (uint256 => Proposal) public proposals;
 
@@ -207,30 +191,24 @@ contract KingAutomaton {
     _;
   }
 
-  function initTreasury() private {}
-
-  function createBallotBox(uint256 _choices) public returns (uint256) {
+  function createBallotBox(uint256 _choices) public returns (uint256 id) {
     require (_choices > 1, "Number of choices must be bigger than 1!");
-    uint256 id = ++ballotBoxIDs;
+    id = ++ballotBoxIDs;
     BallotBox storage b = ballotBoxes[id];
-
     b.numChoices = _choices;
     b.state = BallotBoxState.PrepayingGas;
-
     for (uint256 i = 0; i <= _choices; i++) {
       b.voteCount[i] = MSB_SET;
     }
-
     payForGas(id, 1);
-    return id;
   }
 
-  function createProposal(address payable contributor, string calldata title,
-      string calldata documents_link, bytes calldata documents_hash,
-      uint256 budget_period_len, uint256 num_periods, uint256 budget_per_period) external returns (uint256) {
+  function createProposal(address payable contributor, string calldata title, string calldata documents_link,
+      bytes calldata documents_hash, uint256 budget_period_len, uint256 num_periods, uint256 budget_per_period)
+      external returns (uint256 _id) {
     require(num_periods * budget_per_period <= treasuryLimitPercentage * balances[treasuryAddress] / 100);
-    uint256 id = createBallotBox(2);
-    Proposal storage p = proposals[id];
+    _id = createBallotBox(2);
+    Proposal storage p = proposals[_id];
     p.contributor = contributor;
     p.title = title;
     p.documentsLink = documents_link;
@@ -240,9 +218,7 @@ contract KingAutomaton {
     p.budgetPeriodLen = budget_period_len;
     p.remainingPeriods = num_periods;
     p.budgetPerPeriod = budget_per_period;
-
-    transferInternal(treasuryAddress, address(id), num_periods * budget_per_period);
-    return id;
+    transferInternal(treasuryAddress, address(_id), num_periods * budget_per_period);
   }
 
   function unpaidSlots(uint256 _id) public view validBallotBoxID(_id) returns (uint256) {
@@ -309,8 +285,6 @@ contract KingAutomaton {
     // Incentivize voters by giving them a refund.
     b.payGas1[_slot] = 0;
     b.payGas2[_slot] = 0;
-
-    // updateProposalState(_id);
   }
 
   function getVote(uint256 _id, uint256 _slot) public view validBallotBoxID(_id) returns (uint) {
@@ -324,23 +298,21 @@ contract KingAutomaton {
     uint256 mask = ((1 << bitsPerVote) - 1) << offset;
 
     // Get vote
-    return (ballotBoxes[_id].votes[index] & mask) >> offset;
+    return (b.votes[index] & mask) >> offset;
   }
 
-  function getVoteCount(uint256 _id, uint256 _choice) public view
-      validBallotBoxID(_id) returns(uint256) {
+  function getVoteCount(uint256 _id, uint256 _choice) public view validBallotBoxID(_id) returns(uint256) {
     return ballotBoxes[_id].voteCount[_choice] & ALL_BUT_MSB;
   }
 
   function completeProposal(uint256 _id) private {
     updateProposalState(_id);
-    BallotBox storage b = ballotBoxes[_id];
-    require(b.state == BallotBoxState.Active, "Ballot is not active!");
+    require(ballotBoxes[_id].state == BallotBoxState.Active, "Ballot is not active!");
     Proposal storage p = proposals[_id];
     ProposalState p_state = p.state;
     require(p_state == ProposalState.Started || p_state == ProposalState.Contested, "Invalid proposal state!");
     p.state = ProposalState.Completed;
-    b.state = BallotBoxState.Inactive;
+    ballotBoxes[_id].state = BallotBoxState.Inactive;
   }
 
   function updateProposalState(uint256 _id) public validBallotBoxID(_id) {
@@ -367,16 +339,12 @@ contract KingAutomaton {
         }
       }
     } else if (p_state == ProposalState.Accepted) {
-      // BallotBox storage b = ballotBoxes[_id];
-      // require(b.state == BallotBoxState.Active, "Ballot is not active!");
       int256 vote_diff = calcVoteDifference(_id);
       if (vote_diff <= contestPercentage) {
         p.state = ProposalState.Contested;
         p.contestEndDate = now + CONTEST_PERIOD;
       }
     } else if (p_state == ProposalState.Contested) {
-      // BallotBox storage b = ballotBoxes[_id];
-      // require(b.state == BallotBoxState.Active, "Ballot is not active!");
       if (now >= p.contestEndDate) {
         int256 vote_diff = calcVoteDifference(_id);
         if (vote_diff >= approvalPercentage) {
@@ -483,6 +451,7 @@ contract KingAutomaton {
     uint256 difficulty;
     uint256 last_claim_time;
   }
+
   mapping (uint256 => ValidatorSlot) public slots;
   uint256 public numSlots;
 
@@ -495,7 +464,7 @@ contract KingAutomaton {
     require(nSlots > 0);
     require(minDifficultyBits > 0);
 
-    minDifficulty = (2 ** uint256(minDifficultyBits) - 1) << (256 - minDifficultyBits);
+    minDifficulty = (2 ** minDifficultyBits - 1) << (256 - minDifficultyBits);
     if (predefinedMask == 0) {
       // Prevents premining with a known predefined mask.
       mask = uint256(keccak256(abi.encodePacked(now, msg.sender)));
@@ -503,7 +472,6 @@ contract KingAutomaton {
       // Setup predefined mask, useful for testing purposes.
       mask = predefinedMask;
     }
-
     rewardPerSlotPerSecond = (1 ether * initialDailySupply) / 1 days / nSlots;
   }
 
@@ -559,13 +527,7 @@ contract KingAutomaton {
     * @param r R portion of the signature
     * @param s S portion of the signature
     */
-  function claimSlot(
-    bytes32 pubKeyX,
-    bytes32 pubKeyY,
-    uint8 v,
-    bytes32 r,
-    bytes32 s
-  ) public {
+  function claimSlot(bytes32 pubKeyX, bytes32 pubKeyY, uint8 v, bytes32 r, bytes32 s) public {
     uint256 slot = uint256(pubKeyX) % numSlots;
     uint256 key = uint256(pubKeyX) ^ mask;
 
@@ -655,10 +617,7 @@ contract KingAutomaton {
   // DEX
   //////////////////////////////////////////////////////////////////////////////////////////////////
 
-  uint256 public minOrderETH = 1 ether / 10;
-  uint256 public minOrderAUTO = 1000 ether;
-
-  enum OrderType { Buy, Sell, Auction }
+  enum OrderType {None, Buy, Sell, Auction}
 
   struct Order {
     uint256 AUTO;
@@ -667,56 +626,77 @@ contract KingAutomaton {
     OrderType orderType;
   }
 
-  Order[] public orders;
+  mapping(uint256 => Order) public orders;
+  mapping(address => uint256) public withdraws;
+  uint256 private ids;
+
+  uint256 public constant minOrderETH = 1 ether / 10;
+  uint256 public constant minOrderAUTO = 1000 ether;
+
+  function withdraw(uint256 _value) external {
+    require(_value <= withdraws[msg.sender]);
+    withdraws[msg.sender] -= _value;
+    (bool success, ) = msg.sender.call.value(_value)("");
+    require(success);
+  }
 
   function getDEXBalance() public view returns (uint256) {
     return balanceOf(DEXAddress);
   }
 
   function removeOrder(uint256 _id) private {
-    orders[_id] = orders[orders.length - 1];
-    orders.pop();
+    Order memory null_order;
+    null_order.AUTO = 0;
+    null_order.ETH = 0;
+    null_order.owner = address(0);
+    null_order.orderType = OrderType.None;
+    orders[_id] = null_order;
   }
 
   function getOrdersLength() public view returns (uint256) {
-    return orders.length;
+    return ids;
   }
 
-  function buy(uint256 _AUTO) public payable returns (uint256 _id) {
+  function buy(uint256 _AUTO) public payable returns (uint256 id) {
     require(msg.value >= minOrderETH, "Minimum ETH requirement not met");
     require(_AUTO >= minOrderAUTO, "Minimum AUTO requirement not met");
-    _id = orders.length;
-    orders.push(Order(_AUTO, msg.value, msg.sender, OrderType.Buy));
+    id = ++ids;
+    orders[id] = Order(_AUTO, msg.value, msg.sender, OrderType.Buy);
   }
 
   function sellNow(uint256 _id, uint256 _AUTO, uint256 _ETH) public {
-    require(_id < orders.length, "Invalid Order ID");
     Order memory o = orders[_id];
+    require(o.owner != address(0), "Invalid Order ID");
     require(o.AUTO == _AUTO, "Order AUTO does not match requested size");
     require(o.ETH == _ETH, "Order ETH does not match requested size");
     require(o.orderType == OrderType.Buy, "Invalid order type");
+    uint256 w = withdraws[msg.sender];
+    require(w + _ETH > w);
+    withdraws[msg.sender] += _ETH;
     removeOrder(_id);
     transfer(o.owner, _AUTO);
-    msg.sender.transfer(_ETH);
   }
 
   function sell(uint256 _AUTO, uint256 _ETH) public returns (uint256 _id){
     require(_AUTO >= minOrderAUTO, "Minimum AUTO requirement not met");
     require(_ETH >= minOrderETH, "Minimum ETH requirement not met");
     transfer(DEXAddress, _AUTO);
-    _id = orders.length;
-    orders.push(Order(_AUTO, _ETH, msg.sender, OrderType.Sell));
+     _id = ++ids;
+    orders[_id] = Order(_AUTO, _ETH, msg.sender, OrderType.Sell);
   }
 
   function buyNow(uint256 _id, uint256 _AUTO) public payable {
-    require(_id < orders.length, "Invalid Order ID");
     Order memory o = orders[_id];
+    require(o.owner != address(0), "Invalid Order ID");
     require(o.AUTO == _AUTO, "Order AUTO does not match requested size");
     require(o.ETH == msg.value, "Order ETH does not match requested size");
     require(o.orderType == OrderType.Sell, "Invalid order type");
+    uint256 w = withdraws[o.owner];
+    require(w + msg.value > w);
+    withdraws[o.owner] += msg.value;
     removeOrder(_id);
+
     transferInternal(DEXAddress, msg.sender, _AUTO);
-    o.owner.transfer(msg.value);
   }
 
   function cancelOrder(uint256 _id) public {
@@ -725,7 +705,8 @@ contract KingAutomaton {
     removeOrder(_id);
 
     if (o.orderType == OrderType.Buy) {
-      msg.sender.transfer(o.ETH);
+      require(withdraws[msg.sender] + o.ETH > withdraws[msg.sender]);
+      withdraws[msg.sender] += o.ETH;
     } else if (o.orderType == OrderType.Sell) {
       transferInternal(DEXAddress, msg.sender, o.AUTO);
     }
@@ -734,6 +715,17 @@ contract KingAutomaton {
   //////////////////////////////////////////////////////////////////////////////////////////////////
   // Internal Helpers
   //////////////////////////////////////////////////////////////////////////////////////////////////
+
+  function msb(uint256 x) public pure returns (uint8 r) {
+    if (x >= 0x100000000000000000000000000000000) {x >>= 128; r += 128;}
+    if (x >= 0x10000000000000000) {x >>= 64; r += 64;}
+    if (x >= 0x100000000) {x >>= 32; r += 32;}
+    if (x >= 0x10000) {x >>= 16; r += 16;}
+    if (x >= 0x100) {x >>= 8; r += 8;}
+    if (x >= 0x10) {x >>= 4; r += 4;}
+    if (x >= 0x4) {x >>= 2; r += 2;}
+    if (x >= 0x2) r += 1; // No need to shift x anymore
+  }
 
   // Returns the Ethereum address corresponding to the input public key.
   function getAddressFromPubKey(bytes32 pubkeyX, bytes32 pubkeyY) private pure returns (uint256) {
