ae2071414abdb7419a5c8aa95875847da1e26a55
Asen Kovachev <akovachev@users.noreply.github.com>
2020-03-31 23:46:32 +0300

    Update BuildDependencies.cmake

--- src/cmake/BuildDependencies.cmake
+++ src/cmake/BuildDependencies.cmake
@@ -299,15 +299,6 @@ else()
   )
 endif()
 
-FetchContent_Declare(JUCE
-  GIT_REPOSITORY "https://github.com/WeAreROLI/JUCE.git"
-  GIT_TAG "dddeb1ad68f8539f5ea2fbac36c07532f12bf9cf"
-  UPDATE_COMMAND ""
-#  URL https://github.com/WeAreROLI/JUCE/archive/5.4.7.tar.gz
-#  URL_HASH SHA256=0f446cf09177e559d2f2e9a77a78faed611cc869e219a7dc859a6e9b72eca64d
-#  SOURCE_DIR ${CMAKE_INSTALL_PREFIX}/JUCE
-)
-
 FetchContent_GetProperties(JUCE)
 
 if (NOT JUCE_POPULATED)
