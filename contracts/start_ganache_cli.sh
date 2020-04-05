83a00cb4c8da919f08e5797c6e3cc51d65741e3f
akovachev <asenski@gmail.com>
2020-04-05 20:55:29 +0300

    Update start_ganache_cli.sh

--- contracts/start_ganache_cli.sh
+++ contracts/start_ganache_cli.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-ganache-cli -m 'myth like bonus scare over problem client lizard pioneer submit female collect' -p 7545 -e 10000 &
+ganache-cli -m 'myth like bonus scare over problem client lizard pioneer submit female collect' -p 7545 -e 10000
