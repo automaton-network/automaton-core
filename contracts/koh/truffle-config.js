c84d747e8064b6d8864649390679c33e29e93622
Asen Kovachev <akovachev@users.noreply.github.com>
2020-04-05 22:46:33 +0300

    add truffle/solidity coverage (#201)

--- contracts/koh/truffle-config.js
+++ contracts/koh/truffle-config.js
@@ -38,6 +38,8 @@ module.exports = {
    * $ truffle test --network <network-name>
    */
 
+  plugins: ["solidity-coverage"],
+
   networks: {
     // Useful for testing. The `development` name is special - truffle uses it by default
     // if it's defined here and no other network is specified at the command line.
