db020cde981b9f117cb9c5af21db0d65ab605438
Asen Kovachev <akovachev@users.noreply.github.com>
2020-04-14 08:56:18 +0300

    Delete compile_and_deploy_contracts.sh

--- contracts/compile_and_deploy_contracts.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-
-# solc --combined-json abi,bin koh/contracts/KingAutomaton.sol
-cd koh
-
-truffle --network ganache deploy --reset && truffle --network ganache console  # --verbose-rpc
-# koh = await KingAutomaton.deployed()
-# koh.getMask().then(r=>{console.log(r.toString(16))})
